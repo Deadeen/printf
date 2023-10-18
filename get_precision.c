b18794f9436a08b97a9ac0f8ba6898c154876d1c
Deadeen <141264828+Deadeen@users.noreply.github.com>
2023-10-18 05:23:49 +0300

    no way

--- get_precision.c
+++ get_precision.c
@@ -1,60 +1,42 @@
 #include "main.h"
 
 /**
- * is_digit - Checks if a character is a digit.
- * @c: The character to check.
- *
- * Return: 1 if 'c' is a digit, 0 otherwise.
- */
-int is_digit(char c) {
-    return (c >= '0' && c <= '9');
-}
-
-/**
- * get_precision - Calculates the precision for printing.
- * @format: Formatted string in which to print the arguments.
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
- * @list: List of arguments.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list) {
-    int curr_i = *i + 1;
-    int precision = -1;
+int get_precision(const char *format, int *i, va_list list)
+{
+	int curr_i = *i + 1;
+	int precision = -1;
 
-    if (format[curr_i] != '.')
-        return precision;
+	if (format[curr_i] != '.')
+		return (precision);
 
-    precision = 0;
+	precision = 0;
 
-    for (curr_i += 1; format[curr_i] != '\0'; curr_i++) {
-        if (is_digit(format[curr_i])) {
-            precision *= 10;
-            precision += format[curr_i] - '0';
-        } else if (format[curr_i] == '*') {
-            curr_i++;
-            precision = va_arg(list, int);
-            break;
-        } else {
-            break;
-        }
-    }
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-    *i = curr_i - 1;
+	*i = curr_i - 1;
 
-    return precision;
+	return (precision);
 }
 
-int main() {
-    // Example usage
-    const char *format = "%.3f";
-    int i = 0;
-    va_list args;
-    va_start(args, format);
-    int precision = get_precision(format, &i, args);
-    va_end(args);
-
-    printf("Precision: %d\n", precision);
-
-    return 0;
-}
